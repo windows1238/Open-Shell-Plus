82ddccbd713d590e8f89f8a0f42e1a97d7d359b7
XenHat <XenHat@users.noreply.github.com>
2018-06-19 23:48:20 -0400

    Fix stdafx include

--- ClassicShellSrc/ClassicShellLib/ResourceHelper.cpp
+++ ClassicShellSrc/ClassicShellLib/ResourceHelper.cpp
@@ -1,7 +1,7 @@
 // Classic Shell (c) 2009-2016, Ivo Beltchev
 // Confidential information of Ivo Beltchev. Not for disclosure or distribution without prior written consent from the author
 
-#include <stdafx.h>
+#include "stdafx.h"
 #include "StringSet.h"
 #include "StringUtils.h"
 #include "Settings.h"
