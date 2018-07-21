cc047a3423db2bca4106f41ce8c39d7a746e550d
ge0rdi <ge0rdi@gmx.com>
2018-07-21 11:57:12 +0200

    Build: Less verbose output

--- ClassicStartSrc/ClassicStartSetup/BuildArchives.bat
+++ ClassicStartSrc/ClassicStartSetup/BuildArchives.bat
@@ -1,10 +1,10 @@
 REM ***** Collect PDBs
 
+echo -- Creating symbols package
 set CS_SYMBOLS_NAME=ClassicStartPDB_%CS_VERSION_STR%.7z
 
-if exist Final\%CS_SYMBOLS_NAME% del Final\%CS_SYMBOLS_NAME%
 cd Output
-7z a -mx9 ..\Final\%CS_SYMBOLS_NAME% PDB32 PDB64
+7z a -mx9 ..\Final\%CS_SYMBOLS_NAME% PDB32 PDB64 > nul
 cd ..
 
 if defined APPVEYOR (
@@ -15,9 +15,11 @@ cd ..
 
 REM ***** Collect Localization files
 
-del ClassicStartSetup\Final\ClassicStartLoc.zip
+echo -- Creating localization package
 cd Localization
-7z a -r -x!en-US -x!*WixUI_en-us.wxl -x!*.adml -x!*.admx -x!*LocComments.txt ..\ClassicStartSetup\Final\ClassicStartLoc.zip English ..\ClassicExplorer\ExplorerL10N.ini ..\ClassicStartMenu\StartMenuL10N.ini ..\ClassicStartMenu\StartMenuHelper\StartMenuHelperL10N.ini English\ClassicStartText-en-US.wxl English\ClassicStartEULA.rtf
+7z a -r -x!en-US -x!*WixUI_en-us.wxl -x!*.adml -x!*.admx -x!*LocComments.txt ..\ClassicStartSetup\Final\ClassicStartLoc.zip English ..\ClassicExplorer\ExplorerL10N.ini ..\ClassicStartMenu\StartMenuL10N.ini ..\ClassicStartMenu\StartMenuHelper\StartMenuHelperL10N.ini English\ClassicStartText-en-US.wxl English\ClassicStartEULA.rtf > nul
 cd ..
 
 cd ClassicStartSetup
+
+exit /b 0
