4722c03a16cdd21a656b77addfe9b0cab6cd66b9
ge0rdi <ge0rdi@gmx.com>
2018-07-21 15:36:26 +0200

    Build: Support for version suffix
    AppVeyor may add suffix to version (X.Y.Z-abc) in case of PR builds.

--- ClassicStartSrc/ClassicStartSetup/__MakeFinal.bat
+++ ClassicStartSrc/ClassicStartSetup/__MakeFinal.bat
@@ -18,6 +18,10 @@ echo Version: %CS_VERSION%
 rem Convert . to _
 set CS_VERSION_STR=%CS_VERSION:.=_%
 
+set CS_VERSION_ORIG=%CS_VERSION%
+rem Strip optional "-xyz" suffix from version
+for /f "delims=- tokens=1,1" %%i in ("%CS_VERSION%") do set CS_VERSION=%%i
+
 call BuildBinaries.bat
 if ERRORLEVEL 1 exit /b 1
 
