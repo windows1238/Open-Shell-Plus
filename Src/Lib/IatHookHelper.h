0b9705095368391017d9d44bc54890dd6ead3ca8
Matthijs Lavrijsen <mattiwatti@gmail.com>
2021-02-09 10:37:10 +0100

    Add support for IAT hooking on ARM64

--- Src/Lib/IatHookHelper.h
+++ Src/Lib/IatHookHelper.h
@@ -6,8 +6,12 @@
 
 struct IatHookData
 {
+#if defined(_M_AMD64) || defined(_M_IX86)
 	unsigned char jump[4]; // jump instruction 0x90, 0x90, 0xFF, 0x25
 	DWORD jumpOffs; // jump instruction offset
+#elif defined(_M_ARM64)
+	unsigned char jump[8]; // LDR <address>, BR
+#endif
 	void *newProc; // the address of the new proc
 	void *oldProc; // the address of the old proc
 	IMAGE_THUNK_DATA *thunk; // the IAT thunk
